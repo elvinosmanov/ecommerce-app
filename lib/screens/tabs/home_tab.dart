5d3bf5f00fceb51fabe18d1bbcf3e88302793322
Elvin Osmanov <youngtronik@gmail.com>
2021-02-10 17:31:10 +0400

    beforeCreateModel

---
+++ lib/screens/tabs/home_tab.dart
@@ -0,0 +1,138 @@
+import 'package:ecommmerce_app/constants.dart';
+import 'package:ecommmerce_app/models/product.dart';
+import 'package:ecommmerce_app/screens/product_page.dart';
+import 'package:ecommmerce_app/services/firebase_services.dart';
+import 'package:ecommmerce_app/widgets/custom_action_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class HomeTab extends StatelessWidget {
+  final FirebaseServices _firebaseServices = FirebaseServices();
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        FutureBuilder<QuerySnapshot>(
+          future: _firebaseServices.producRef.get(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return Container(
+                child: Center(child: Text("Error:" + snapshot.error)),
+              );
+            }
+            if (snapshot.connectionState == ConnectionState.done) {
+              List<Product> productList = snapshot.data.docs.map((document) {
+                return Product.fromMap(document);
+              });
+              return ListView(
+                padding: EdgeInsets.only(top: 100.0),
+                children: productList.map(
+                  (document) {
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ProductPage(
+                                      productId: document.id,
+                                      productData: document.data(),
+                                    )));
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12)),
+                        height: 350.0,
+                        margin: EdgeInsets.symmetric(
+                          vertical: 12.0,
+                          horizontal: 24.0,
+                        ),
+                        child: Stack(
+                          children: [
+                            Container(
+                              height: 350.0,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(12),
+                                child: Image.network(
+                                  document.data()['images'][0],
+                                  fit: BoxFit.cover,
+                                  loadingBuilder: (BuildContext context,
+                                      Widget child,
+                                      ImageChunkEvent loadingProgress) {
+                                    if (loadingProgress == null) return child;
+                                    return Container(
+                                      color: Colors.grey[300],
+                                      child: Center(
+                                        child: CircularProgressIndicator(
+                                          value: loadingProgress
+                                                      .expectedTotalBytes !=
+                                                  null
+                                              ? loadingProgress
+                                                      .cumulativeBytesLoaded /
+                                                  loadingProgress
+                                                      .expectedTotalBytes
+                                              : null,
+                                        ),
+                                      ),
+                                    );
+                                  },
+                                ),
+                              ),
+                            ),
+                            Positioned(
+                              bottom: 0.0,
+                              left: 0,
+                              right: 0,
+                              child: Padding(
+                                padding: const EdgeInsets.all(24.0),
+                                child: Container(
+                                  padding: EdgeInsets.symmetric(
+                                      vertical: 4.0, horizontal: 8.0),
+                                  decoration: BoxDecoration(
+                                    color: Colors.white.withOpacity(0.4),
+                                    borderRadius: BorderRadius.circular(8),
+                                  ),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        document.data()['name'] ??
+                                            'Product Name',
+                                        style: Constants.boldHeading,
+                                      ),
+                                      Text(
+                                        '\$${document.data()['price']}' ??
+                                            'Price',
+                                        style: TextStyle(
+                                          fontSize: 22.0,
+                                          fontWeight: FontWeight.w600,
+                                          color: Theme.of(context).accentColor,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ).toList(),
+              );
+            }
+            return Container(
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          },
+        ),
+        CustomActionBar(
+          title: "Home",
+        ),
+      ],
+    );
+  }
+}
